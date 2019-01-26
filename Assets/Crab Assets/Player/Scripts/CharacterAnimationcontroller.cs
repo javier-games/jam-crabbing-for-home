0899e80f7f50607b9cc21abbabc543565666f14d
David <dakalodev@gmail.com>
2019-01-26 17:36:57 -0600

    Prefab y tieles

---
+++ Assets/Crab Assets/Player/Scripts/CharacterAnimationcontroller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class CharacterAnimationcontroller : MonoBehaviour
+{
+
+    private Animator playerAnimator;
+
+     private void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation( int animIndex)
+    {
+        switch (animIndex)
+        {
+            // idle
+            case 0:
+                break;
+                //walk
+            case 1:
+                break;
+                // glide
+            case 2:
+                break;
+            //jump
+
+            case 3:
+                break;
+            //death
+            case 4:
+                break;
+                //default
+            default:
+                break;
+        }
+    }
+}
