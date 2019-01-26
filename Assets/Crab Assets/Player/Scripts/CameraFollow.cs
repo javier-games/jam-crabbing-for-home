48fa2c5f4fb59ea7376bdd2d3e08aef7882cdb4c
David <dakalodev@gmail.com>
2019-01-26 14:27:39 -0600

    Camera flow & map

---
+++ Assets/Crab Assets/Player/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private float smoothSpeed;
+    [SerializeField] private Vector3 cameraOffset;
+
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = target.transform.position + cameraOffset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, pos, smoothSpeed);
+        transform.position = smoothPos;
+        
+    }
+}
