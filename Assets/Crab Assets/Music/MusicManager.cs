cdee2948a0128d1c85c3fb3c3515c73c95c2016a
David <dakalodev@gmail.com>
2019-01-27 15:21:25 -0600

    va nuevo

---
+++ Assets/Crab Assets/Music/MusicManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioClip currentClip;
+
+    private void ChangeMusic()
+    {
+
+    }
+    
+}
