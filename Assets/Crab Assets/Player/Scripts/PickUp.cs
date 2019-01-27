748dc8b12fc035c7fd9b7e7eec9e81c3f44faa05
David <dakalodev@gmail.com>
2019-01-26 21:18:48 -0600

    Animaciones

---
+++ Assets/Crab Assets/Player/Scripts/PickUp.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField] private GameObject homeAnchor;
+    [SerializeField] private GameObject homeAnchorFliped;
+
+    private bool hasItem = false;
+    public static GameObject pickedObject;
+    private SpriteRenderer[] sprites;
+   
+
+
+
+    private void OnEnable()
+    {
+        PlayerController.flipAction += Flipping;
+    }
+    private void OnDisable()
+    {
+        PlayerController.flipAction -= Flipping;
+
+    }
+
+    private void Start()
+    {
+        sprites = new SpriteRenderer[2];
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.transform.tag == "Pickable")
+        {
+            if (!hasItem)
+            {
+                pickedObject = collision.transform.gameObject;
+                sprites = collision.GetComponentsInChildren<SpriteRenderer>();
+                SetHome();
+            }          
+        }
+    }
+
+    private void SetHome()
+    {
+       Collider2D coll = pickedObject.GetComponent<Collider2D>();
+        coll.enabled = false;
+        pickedObject.transform.parent = homeAnchor.transform;
+        pickedObject.transform.position = homeAnchor.transform.position;
+        pickedObject.transform.rotation = homeAnchor.transform.rotation;
+    }
+
+    private void Flipping(bool flip)
+    {
+        if (flip)
+        {
+            pickedObject.transform.parent = homeAnchorFliped.transform;
+            pickedObject.transform.position = homeAnchorFliped.transform.position;
+            pickedObject.transform.rotation = homeAnchorFliped.transform.rotation;
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i].flipX = true;
+            }
+        }
+        else
+        {
+            pickedObject.transform.parent = homeAnchor.transform;
+            pickedObject.transform.position = homeAnchor.transform.position;
+            pickedObject.transform.rotation = homeAnchor.transform.rotation;
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i].flipX = false;
+            }
+        }
+    }
+}
