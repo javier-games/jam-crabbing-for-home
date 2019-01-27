7f384d445c88e801f2d9aec2de4f3f28bbe71909
David <dakalodev@gmail.com>
2019-01-27 16:50:14 -0600

    vamamama

--- Assets/Crab Assets/Music/MusicManager.cs
+++ Assets/Crab Assets/Music/MusicManager.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    private AudioClip currentClip;
+    [SerializeField] private AudioClip [] audios;
+    [SerializeField] AudioSource audioSource;
+    AudioClip currentAudio;
 
-    private void ChangeMusic()
+    private void Start()
     {
+        currentAudio = GetComponent<AudioClip>();
+    }
 
+    public void ChangeMusic(int audioToPlay)
+    {
+         
+        if (audioToPlay <= audios.Length)
+        {
+
+            currentAudio = audios[audioToPlay];
+            audioSource.clip = currentAudio;
+            audioSource.Play();
+            
+        }
+
+     
     }
     
 }
---
+++ Assets/Crab Assets/Music/soundCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundCollider : MonoBehaviour
+{
+    [SerializeField] private int soundIndex;
+    [SerializeField] private MusicManager musicaManager;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            musicaManager.ChangeMusic(soundIndex);
+            Collider2D col = GetComponent<Collider2D>();
+            col.enabled = false;
+        }
+    }
+}
